869c0b812c8bf8288b553697fea3a6a2aaa9ce40
damtaipu <damiaotaipu@gmail.com>
2022-08-05 18:14:42 -0300

    Let small code

--- consumer/index.js
+++ consumer/index.js
@@ -1,13 +1,35 @@
-const express = require('express');
-const callKafka = require('./express');
+import express from 'express';
+import { Kafka } from 'kafkajs';
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:9092'],
+});
 
 const app = express();
-new callKafka();
+
+// Consumer
+const runConsumer = async function () {
+  const consumer = kafka.consumer({ groupId: 'test-group' });
+  await consumer.connect();
+  await consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      })
+    },
+  });
+}
 
 app.get('/', function (req, res) {
   res.send('Hello World')
-})
+});
 
 app.listen(3042, () => {
-    console.log('Server is up')
-})
+  runConsumer();
+  console.log('Server Consumer is UP');
+});
--- producer/index.js
+++ producer/index.js
@@ -1,13 +1,31 @@
-const express = require('express');
-const callKafka = require('./express');
+import express from 'express';
+import { Kafka } from 'kafkajs';
 
 const app = express();
 
+const kafkaCall = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9092']
+});
+
+// Producer
+const runProducer = async function(){
+    const producer = kafkaCall.producer();
+    await producer.connect();
+    await producer.send({
+        topic: 'test-topic',
+        messages: [
+            { value: 'Olá! Acabei essa minha primeira menssagem com Kafka.'+Math.random()}
+        ],
+    });
+    await producer.disconnect();
+}
+
 app.get('/', function (req, res) {
-    new callKafka();
-    res.send('Hello World')
+    runProducer();
+    res.send('Enviando evento via Kafka');
 })
 
 app.listen(3040, () => {
-    console.log('Server is up')
+    console.log('Server Producer is UP');
 })
